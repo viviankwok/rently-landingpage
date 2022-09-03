dc7379693e574b804410beb7ab75422ec4e8af4a
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-09-04 00:25:28 +0800

    updated userData

--- client/src/data/userData.js
+++ client/src/data/userData.js
@@ -1,8 +1,35 @@
 const userData = [
-  { phoneNumber: 0, password: "testing0" },
-  { phoneNumber: 1, password: "testing1" },
-  { phoneNumber: 2, password: "testing2" },
-  { phoneNumber: 3, password: "testing3" },
+  {
+    firstName: "First0",
+    lastName: "Last0",
+    email: "email0@gmail.com",
+    phoneNumber: "0",
+    password: "testing0",
+  },
+
+  {
+    firstName: "First1",
+    lastName: "Last1",
+    email: "email1@gmail.com",
+    phoneNumber: "1",
+    password: "testing1",
+  },
+
+  {
+    firstName: "First2",
+    lastName: "Last2",
+    email: "email2@gmail.com",
+    phoneNumber: "2",
+    password: "testing2",
+  },
+
+  {
+    firstName: "First3",
+    lastName: "Last3",
+    email: "email3@gmail.com",
+    phoneNumber: "3",
+    password: "testing3",
+  },
 ];
 
 export default userData;
