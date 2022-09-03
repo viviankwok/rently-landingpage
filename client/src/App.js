0946b49d3a6f55db14b7a44506ea7d4d5ce99a54
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-09-03 17:44:26 +0800

    set-up client, tailwind, daisyUI

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Child from "./components/Child";
+
+function App() {
+  return (
+    <div className="App">
+      App component
+      <Child />
+      <button class="btn btn-success">testing</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Child.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Child = () => {
+  return <div>child component</div>;
+};
+
+export default Child;
