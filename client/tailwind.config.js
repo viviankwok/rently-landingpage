b521423b641a32716da2731a3012909056c159ec
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-09-03 23:25:54 +0800

    set colour theme, font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,4 +5,17 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#4aade8",
+          secondary: "#57567b",
+          accent: "#f27a2b",
+          // neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
 };
